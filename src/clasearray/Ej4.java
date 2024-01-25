a361c6ad400c37192e81e490469136faf7d5ac54
yplopez <yuney.lopez@iesnervion.es>
2024-01-25 14:38:05 +0100

    avances 2do boletin

--- src/clasearray/Ej4.java
+++ src/clasearray/Ej4.java
@@ -1,9 +1,32 @@
 package clasearray;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ej4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Creo la variable número para almacenar los números aleatorios
+		int numero;
+		
+		//Crero una tabla y le asigno la longitud
+		int tabla [] = new int [30];
+		
+		//Hago un bucle for de la longitud de la tabla, se generará un número
+		//aleatorio para cada espacio y se almacenará.
+		for (int i=1; i < tabla.length; i++) {
+			numero = (int) (Math.random()*10);
+			tabla[i] = numero;
+		}
+		//Uso la función sorts de la clase arrays para ordenar los valores
+		//de la tabla de menor a mayor.
+		Arrays.sort(tabla);
+		
+		//Imprimo la tabla usando la funcion to.String de la clase arrays
+		System.out.println(Arrays.toString(tabla));
+		
+		
+		
 
 	}
 
---
+++ src/clasearray/Ej5.java
@@ -0,0 +1,10 @@
+package clasearray;
+
+public class Ej5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
